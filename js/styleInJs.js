cf159d5abfa5a80ffb24c84988af67f34e032769
ApurboShib <shibjoy088@gmail.com>
2025-01-12 23:24:03 +0600

    make change with js

---
+++ js/styleInJs.js
@@ -0,0 +1,18 @@
+// sob section eh amra kichu ekta change korte cai
+
+const section = document.querySelectorAll('section');
+//console.log(section);
+for(const x of section) {
+    // console.log(x);
+    // amra ekhan theke sections er moddhe change korte pari.
+    x.style.border = '1px solid  blue';
+    x.style.marginBottom = '5px';
+    x.style.borderRadius = '10px';
+    x.style.paddingLeft = '10px';
+    x.style.backgroundColor = 'lightgray';
+
+}
+
+const placeContainer = document.getElementById('place-container')
+
+placeContainer.style.backgroundColor = 'yellow';
